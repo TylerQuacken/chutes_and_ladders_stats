307b2d3a8f66f84642dac54a1d7efe52e9b6ec3d
Tyler Quackenbush <tylerquacken@gmail.com>
2021-12-03 21:08:43 -0800

    Added better plotting, cleaned code

--- calc_value_map_choice.py
+++ calc_value_map_choice.py
@@ -5,12 +5,13 @@ from copy import deepcopy
 
 nStates = 109
 tileMap = np.arange(nStates)
-mapFrom =   [1,  4,  9,  21, 28, 36, 48, 49, 51, 56, 62, 64, 71, 80,  87, 93, 95, 98]
-mapTo =     [38, 14, 31, 42, 84, 44, 26, 11, 67, 53, 18, 60, 91, 100, 24, 73, 76, 78]
+mapFrom = [1,  4,  9,  21, 28, 36, 48, 49, 51, 56, 62, 64, 71, 80,  87, 93, 95, 98]
+mapTo =   [38, 14, 31, 42, 84, 44, 26, 11, 67, 53, 18, 60, 91, 100, 24, 73, 76, 78]
 tileMap[mapFrom] = mapTo
 tileMap[101:106] = 99
 
-valueMap = np.zeros(nStates) + 100
+winReward = 100
+valueMap = np.zeros(nStates) + winReward
 newVal = np.zeros(nStates)
 policy = np.zeros(nStates)
 
@@ -29,11 +30,11 @@ while totalChange > convergeTol and iter < maxIter:
 
         bestChoice = np.max(choices)
         policy[i] = np.argmax(choices)
-        newVal[i] = bestChoice *0.9
+        newVal[i] = bestChoice * 0.9
                 # print("{}: {}".format(i, tileMap[i+1:i+6]))
     
-    newVal[100] = 100
-    newVal[80] = 100
+    newVal[100] = winReward
+    newVal[80] = winReward
     newVal[101:] = newVal[99]
     # newVal[101:] = 100
 
@@ -44,5 +45,7 @@ while totalChange > convergeTol and iter < maxIter:
 fig, axs = plt.subplots(2)
 
 axs[0].plot(valueMap)
+axs[0].set_ylabel('Value')
+axs[1].set_ylabel('Optimal Action')
 axs[1].plot(policy + 1)
 plt.show()
